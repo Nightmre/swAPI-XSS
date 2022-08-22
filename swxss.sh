a56adbdd4e3d2ead697aa403439b48b0f438cb04
Nightmare <ksalhse@gmail.com>
2022-08-22 14:37:43 +0300

    Update swxss.sh

--- swxss.sh
+++ swxss.sh
@@ -8,5 +8,5 @@ fi
 
 for i in `cat $1`;
         do echo $i ; subfinder -all -d $i 2> /dev/null | httpx  2> /dev/null | tee $1_alive.txt > /dev/null
-        ffuf -w "$1_alive.txt:URL" -w swagger.txt -u URLFUZZ -mr "Swagger UI" | notify
+        ffuf -w "$1_alive.txt:URL" -w swagger.txt -u URLFUZZ -mr "Swagger UI"
 done
